0ee9ce7242cccf44c7ad0640dfbb465812f0a597
Alex <alexandrareginawilson@gmail.com>
2019-04-08 21:19:46 -0500

    Most html in place.

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+		name: 'Brian, my dog',
+		photo: 'https://unsplash.com/photos/K4mSJ7kc0As',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1'
+		]
+    }
+];
+
+module.exports = friendsArray;
